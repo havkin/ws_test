d3464cc07c69c1382e7f7639e93f509db0bae2fb
Max Sozinov <max.sozinov@cupcakedev.com>
2021-06-01 14:00:05 +0700

    add data for announcement_create message

--- src/setup/messages.js
+++ src/setup/messages.js
@@ -26,7 +26,17 @@ export const messages = {
   },
   announcement_create: {
     type: "announcement_create",
-    data: {},
+    data: {
+      playerUIDs: [
+        "25c8e098d0a163d78fb79f9ec6c8333eac48d360488aa01e7b343f938740d206",
+        "648cea44b0d747d233c7b2c061e63b08465e98d937601640a23221d6266ab201",
+      ],
+      message: "{{role.police_officer.players.[0].UID}}",
+      toLog: false,
+      actionGroupName: "Reveal",
+      actionGroupActionId: 0,
+      actionName: "first test",
+    },
     isVisible: true,
   },
   role_confirm: {
